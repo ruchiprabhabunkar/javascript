f6c9f186efbd50c074a848485fb95482f833b0d1
ruchiprabhabunkar <138413675+ruchiprabhabunkar@users.noreply.github.com>
2025-01-17 08:26:30 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score="ruchi"
+
+// console.log(typeof score);
+
+
+let valueInNumber= Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33"=> 33
+//"33abc"=> NaN
+//true=>1 ;false=>0
+
+let isLoggedIn="ruchi"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1=> true; 0=>false
+// ""=>false
+//"ruchi" => true
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
