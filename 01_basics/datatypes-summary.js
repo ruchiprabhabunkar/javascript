b4303283bbb82258f1edf6b2c80dbceeb8772d21
Ruchi <prabharuchi09@gmail.com>
2025-01-17 22:20:35 +0530

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt,
+
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLoggedIn = false
+ const outsideTemp = null
+let userEmail;
+
+
+const id = Symbol(123)
+const anotherId = Symbol('123')
+// console.log(id ===anotherId);
+
+const bigNumber = 263774567589458974927578758795794375n
+// console.log(bigNumber);
+
+
+
+// Refrence Types (Non primitive)
+
+// Arrays, Objects, Functions
+
+const heroes = ["shaktiman" , "naagraj" , "doga"]
+let myObj={
+    name : "ruchi",
+    age : 24,
+
+}
+const myFunction = function (){
+console.log("Hello World");
+}
+
+ console.log(typeof myObj);
+
+ //ecmascript tc39 documentSummary
+ 
