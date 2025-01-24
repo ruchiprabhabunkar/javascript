7f02b85af8fbbb2b3f8d0ca45883e89d44b8fe46
Ruchi <prabharuchi09@gmail.com>
2025-01-24 20:12:19 +0530

    iteration

---
+++ 05_iterations/one.js
@@ -0,0 +1,32 @@
+// for
+
+for (let i = 0; i < 10; i++) {
+  const element = i;
+  if (element == 5) {
+    //    console.log("5 is best number");
+  }
+  // console.log(element);
+}
+
+//console.log(element);
+
+for (let i = 1; i <= 10; i++) {
+ // console.log(`outer loop value ${i}`);
+
+  for (let j = 1; j <= 10; j++) {
+    //console.log(`Inner loop values ${j} and inner loop ${i}`);
+    //console.log(i + '*' + j + ' = ' + i*j);
+  }
+}
+let myArray = ["flash", "batsman", "superman"];
+for (let index = 0; index < myArray.length; index) {
+  const element = myArray[index];
+ // console.log(element);
+}
+
+
+// break and conti
+
+for (let index = 1; index <= 20; index++) {
+   console.log(`value of i is ${index}`);  
+}
