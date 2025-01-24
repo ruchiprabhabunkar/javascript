827f01bc0490a462b8e21c3b501e829f6eebc01e
Ruchi <prabharuchi09@gmail.com>
2025-01-24 20:18:35 +0530

    itteration

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -27,6 +27,25 @@ for (let index = 0; index < myArray.length; index) {
 
 // break and conti
 
+
+
+// for (let index = 1; index <= 20; index++) {
+//  if(index == 5){
+//   console.log(`Detected 5`);
+//   break
+  
+//  }
+
+//    console.log(`value of i is ${index}`);  
+// }
+
+
 for (let index = 1; index <= 20; index++) {
+ if(index == 5){
+  console.log(`Detected 5`);
+  continue
+ }
+
    console.log(`value of i is ${index}`);  
 }
+
